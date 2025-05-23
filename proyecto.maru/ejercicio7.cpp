ccc3b73b97d4ef13dcefdd3958332218c29e8b66
MARIANELA MOURAZOS <MARIANELA MOURAZOS@LAPTOP-QDJLQ3BA>
2025-05-23 10:47:28 -0300

    ej8

--- proyecto.maru/ejercicio7.cpp
+++ proyecto.maru/ejercicio7.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 
 int main() {
@@ -30,4 +30,4 @@ int main() {
 
     return 0;
 }
-
+*/
--- proyecto.maru/ejercio8.cpp
+++ proyecto.maru/ejercio8.cpp
@@ -1,4 +1,23 @@
 /*#include <iostream>
 using namespace std;
 
-int main() {}*/
+int main() {
+	int fecha;
+	cout << "Ingrese una fecha en formato AAAAMMDD: " << endl; 
+	cin >> fecha; 
+	if (fecha< 10000000 || fecha> 99999999) { 
+		cout << "error, la fecha debe tener 8 digitos"; 
+		return 1; 
+	}
+	int anio = fecha / 10000;
+	int mes = (fecha / 100) % 100;
+	int dia = fecha % 100;
+
+	cout << "Anio: " << anio << endl;
+	cout << "Mes: " << mes << endl;
+	cout << "Dia: " << dia << endl;
+
+	return 0; 
+
+}
+*/
